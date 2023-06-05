b091e7449e62fc71ac16665ea6c46ef523117ddd
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-06-05 11:17:18 +0300

    lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,25 @@
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
-import CardList from './CardList/CardList';
-import Searchbar from './Searchbar/Searchbar';
-import Home from './Home/Home';
+import { lazy, Suspense } from 'react';
+
+// import CardList from './CardList/CardList';
+// import Searchbar from './Searchbar/Searchbar';
+// import Home from './Home/Home';
+
+const CardList = lazy(() => import('./CardList/CardList'));
+const Searchbar = lazy(() => import('./Searchbar/Searchbar'));
+const Home = lazy(() => import('./Home/Home'));
 
 export const App = () => {
   return (
-    <Router>
+    <Router basename="">
       <>
         <Searchbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tweets" element={<CardList />} />
-        </Routes>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tweets" element={<CardList />} />
+          </Routes>
+        </Suspense>
       </>
     </Router>
   );
