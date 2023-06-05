a39aeb052978fa7a4d727ec113ed39c223967d50
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-06-05 13:31:39 +0300

    vc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,8 +16,8 @@ export const App = () => {
         <Searchbar />
         <Suspense fallback={<p>Loading...</p>}>
           <Routes>
-            <Route path="/test" element={<Home />} />
-            <Route path="/test/tweets" element={<CardList />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/tweets" element={<CardList />} />
           </Routes>
         </Suspense>
       </>
