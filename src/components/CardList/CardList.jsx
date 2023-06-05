bd7c6c825ef78db72c67faa46b9a6a42ff5c5bb2
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-06-05 10:52:46 +0300

    com

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import Card from 'components/Card/Card';
 import { List, LoadMoreButton, Wrapper } from './CardList.styled';
